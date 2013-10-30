48f19d6f19f5b2b29158144ddbd15ae126fcbea6
Wouter Vandenneucker <wouter@vandenneucker.be>
2013-10-30 18:34:09 +0100

    added comments to the code

--- extension/src/js/login.js
+++ extension/src/js/login.js
@@ -1,31 +1,19 @@
-//url = localStorage["url"];
-//encryptionpass = localStorage["encryptionpass"];
-//serviceusername = localStorage["serviceusername"];
-//personalusername = localStorage["personalusername"];
-//
-//url = "https://uhasselt-wifi.uhasselt.be/cgi-bin/login";
-//data = "user="+name+"&password="+pass+"&cmd=authenticate&login=Log In";
 
-//document.getElementById("main").innerHTML = "Logging in on the network!";
-
-//window.alert(data);
-//loadXMLDoc();
-//response;
-
+//Run the init script
 init();
 
+//Add a listener for the submit button
 document.querySelector('#submit').addEventListener('click', submitForm);
 
 
+//Listen for the ENTER-key getting pressed
+//If pressed, submit the form
 if (document.layers) {
   document.captureEvents(Event.KEYDOWN);
 }
-
 document.onkeydown = function (evt) {
   var keyCode = evt ? (evt.which ? evt.which : evt.keyCode) : event.keyCode;
   if (keyCode == 13) {
-    // For Enter.
-    // Your function here.
     document.getElementById('submit').click();
   }
   else {
@@ -33,11 +21,51 @@ document.onkeydown = function (evt) {
   }
 };
 
+
+
+
+
+
+
+
+
+
+/****************************************************************/
+/*                                                              */
+/*    Helper functions that actually do all the work...         */
+/*                                                              */
+/****************************************************************/
+
+//Init function.
+function init(){
+    
+    //Get the current tab
+    chrome.tabs.getSelected(null, function(tab) {
+        //Get the url from the current tab, strip the domain & insert the domain in the html document
+	tabUrl = tab.url;
+        tabDomain = tabUrl.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
+        document.getElementById("domain-field").value = tabDomain;
+
+        //get the settings from the localStorage
+        document.getElementById("serviceusername-field").value = localStorage["serviceusername"];
+        document.getElementById("personalusername-field").value = localStorage["personalusername"];
+        
+        //set focus on the personalpass field (doesn't seem to work)
+        document.getElementById("personalpass-field").focus();
+    });
+}
+
+//Submit function
 function submitForm(){
+    //wrap the vars
     personalusername = document.getElementById("personalusername-field").value;
     personalpassword = document.getElementById("personalpass-field").value;
     
-    var xmlhttp;
+    //make an Ajax request
+    var xmlhttp = new XMLHttpRequest();
+    
+    //Comment below.. Chrome only
+    /*var xmlhttp;
     if (window.XMLHttpRequest){
       // code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
@@ -45,22 +73,31 @@ function submitForm(){
     else{
       // code for IE6, IE5
       xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
+    }*/
+
     
-    
-    
+    //Construt the personal keyword
     var keyword = personalusername + ":" + personalpassword;
     keyword = CryptoJS.SHA1(keyword);
-    timestamp = Math.round(new Date().getTime() / 1000);
     
+    //Add a timestamp
+    timestamp = Math.round(new Date().getTime() / 1000);
     keywordAndTimestamp = keyword + ":" + timestamp;
+    
+    //AES Encrypt the keyword and timestamp with the default AES key from the options
     encryptionpass = localStorage["encryptionpass"];
     theKey = Aes.Ctr.encrypt(keywordAndTimestamp, encryptionpass, 256);
     theKey = encodeURIComponent(theKey); //convert + signs to %2B
+    
+    //Prepare the data and submit to the url from the options page
     data = "key=" + theKey;
     xmlhttp.open("POST", localStorage["url"], true);
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
     xmlhttp.send(data);
+    
+    //Wait until we've got a response
+    //Handle the response
+    //Close the window
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
             response = JSON.parse(xmlhttp.responseText);
@@ -71,6 +108,9 @@ function submitForm(){
     }
 }
 
+
+//Response handling function
+//if not authenticated, throw an alert, else generate the password for this service
 function handleResponse(response, timestamp){
     if(response == "not authenticated!"){
         window.alert("You are: " + response);
@@ -79,56 +119,38 @@ function handleResponse(response, timestamp){
     }
 }
 
+//Password constructer function
 function constructPassword(data, timestamp){
-    timestamp = JSON.stringify(timestamp); //just serialize and convert to string;
+    //stringify the timestamp (timestamp is an object, chrome bitches if you want to encrypt/decrypt using objects as keys)
+    //decrypt the salt with the timestamp
+    timestamp = JSON.stringify(timestamp);
     salt = Aes.Ctr.decrypt(data, timestamp, 256);
  
+    //Sha the default username and the domain
     shaofuname = CryptoJS.SHA1(document.getElementById("serviceusername-field").value);
     shaofdomain = CryptoJS.SHA1(document.getElementById("domain-field").value);
     
+    //Create the key for the password and sha it to get the service pass
     passbase = shaofuname + shaofdomain + salt;
     servicepass = CryptoJS.SHA1(passbase);
     
+    
+    //Copy the service password to the clipboard using some hackerish tricks:
+    //set the value of an invisible inputfield to the servicepass and copy that
     document.getElementById("servicepass").value = servicepass;
     copy();
     
 }
 
+//copy function
+//copies the content of #servicepass to the clipboard.
 function copy() {
-
     //Get Input Element
     var copyDiv = document.getElementById('servicepass');
-
     //Give the text element focus
     copyDiv.focus();
-
     //Select all content
     document.execCommand('SelectAll');
-
     //Copy Content
     document.execCommand("Copy", false, null);
-}
-
-function init(){
-    chrome.tabs.getSelected(null, function(tab) {
-	tabId = tab.id;
-	tabUrl = tab.url;
-        tabDomain = tabUrl.match(/^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/)[1];
-        document.getElementById("domain-field").value = tabDomain;
-
-        document.getElementById("serviceusername-field").value = localStorage["serviceusername"];
-        document.getElementById("personalusername-field").value = localStorage["personalusername"];
-        document.getElementById("personalpass-field").focus();
-    });
-}
-                
-function loadXMLDoc(){
-    
-    xmlhttp.open("POST",url,false);
-    //xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
-    xmlhttp.send(data);
-    //document.getElementById("main").innerHTML = "Logged In!";
-    //document.getElementById("main").innerHTML = xmlhttp.responseText;
-    //response = xmlhttp.responseText;
-    //console.log(response);
 }
--- extension/src/js/options.js
+++ extension/src/js/options.js
@@ -1,43 +1,53 @@
-                document.onload = init();
-                        
-                        function init(){
-                                restoreOptions();          
-                        }
-                        
-                        function restoreOptions() {
-                                var url = localStorage["url"];
-                                var encryptionpass = localStorage["encryptionpass"];
-                                var serviceusername = localStorage["serviceusername"];
-                                var personalusername = localStorage["personalusername"];
-                                if(url != null && encryptionpass != null){
-                                        document.getElementById("url-field").value = url;
-                                        document.getElementById("encryptionpass-field").value = encryptionpass;
-                                }
-                                if(serviceusername != null){
-                                        document.getElementById("serviceusername-field").value = serviceusername;
-                                }
-                                if(personalusername != null){
-                                        document.getElementById("personalusername-field").value = personalusername;
-                                }
-                        }
-                        
-                        document.querySelector('#submit').addEventListener('click', saveOptions);
-                        
-                        function saveOptions() {
-                          
-                          var url = document.getElementById("url-field").value;
-                          var encryptionpass = document.getElementById("encryptionpass-field").value;
-                          var serviceusername = document.getElementById("serviceusername-field").value;
-                          var personalusername = document.getElementById("personalusername-field").value;
-                          
-                          localStorage["url"] = url;
-                          localStorage["encryptionpass"] = encryptionpass;
-                          localStorage["serviceusername"] = serviceusername;
-                          localStorage["personalusername"] = personalusername;
+//Run the initial script on page load.
+document.onload = restoreOptions();
 
-                          //document.getElementById("status").innerHTML = "Saved!";
-                          
-                          //window.alert("saved");
-                          
-                          self.close();
-                        }
+//Make a listener that acts when someone clicks the submit button
+document.querySelector('#submit').addEventListener('click', saveOptions);
+
+
+
+
+/****************************************************************/
+/*                                                              */
+/*    Helper functions that actually do all the work...         */
+/*                                                              */
+/****************************************************************/
+
+//Run the initial script that restores the options
+function restoreOptions() {
+        
+        //Get everything from localStorage and put in in variables.
+        var url = localStorage["url"];
+        var encryptionpass = localStorage["encryptionpass"];
+	var serviceusername = localStorage["serviceusername"];
+	var personalusername = localStorage["personalusername"];
+        
+        //So if those vars are not empty, put them in the input fields
+	if(url != null && encryptionpass != null){
+		document.getElementById("url-field").value = url;
+		document.getElementById("encryptionpass-field").value = encryptionpass;
+	}
+	if(serviceusername != null){
+		document.getElementById("serviceusername-field").value = serviceusername;
+	}
+	if(personalusername != null){
+		document.getElementById("personalusername-field").value = personalusername;
+	}
+}
+
+//Save the options to the localStorage
+function saveOptions(){
+        
+        //Put inputfield values in variables
+        var url = document.getElementById("url-field").value;
+        var encryptionpass = document.getElementById("encryptionpass-field").value;
+	var serviceusername = document.getElementById("serviceusername-field").value;
+	var personalusername = document.getElementById("personalusername-field").value;
+        
+        //Store variables in localStorage
+	localStorage["url"] = url;
+	localStorage["encryptionpass"] = encryptionpass;
+	localStorage["serviceusername"] = serviceusername;
+	localStorage["personalusername"] = personalusername;  
+	self.close();
+}
